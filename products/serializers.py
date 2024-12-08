de194794f3421aa3cfcd949035979e59fd864c52
Carlos Blanco <carlosblancosierra@gmail.com>
2024-12-08 15:58:25 +0100

    Product Gallery Serializer

--- products/serializers.py
+++ products/serializers.py
@@ -1,15 +1,51 @@
 from rest_framework import serializers
-from .models import Product, ProductCategory
-from flavours.serializers import FlavourSerializer
+from .models import Product, ProductCategory, ProductGalleryImage
 
 class ProductCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductCategory
         fields = '__all__'
 
+class ProductGalleryImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductGalleryImage
+        fields = [
+            'id',
+            'image',
+            'image_webp',
+            'thumbnail',
+            'thumbnail_webp',
+            'alt_text',
+            'order'
+        ]
+
 class ProductSerializer(serializers.ModelSerializer):
     category = ProductCategorySerializer()
+    gallery_images = ProductGalleryImageSerializer(many=True, read_only=True)
 
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = [
+            'id',
+            'name',
+            'description',
+            'category',
+            'base_price',
+            'stripe_price_id',
+            'slug',
+            'weight',
+            'active',
+            'sold_out',
+            'units_per_box',
+            'main_color',
+            'secondary_color',
+            'seo_title',
+            'seo_description',
+            'image',
+            'image_webp',
+            'thumbnail',
+            'thumbnail_webp',
+            'gallery_images',
+            'created',
+            'updated'
+        ]
