83321bcca21cd69649c891b00c1b52903b3b0eb7
Carlos Blanco <carlosblancosierra@gmail.com>
2025-01-06 17:14:44 +0100

    Feat - Admin Shipping Company

--- shipping/admin.py
+++ shipping/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ShippingOption
+from .models import ShippingOption, ShippingCompany
 
 # Register your models here.
 admin.site.register(ShippingOption)
+admin.site.register(ShippingCompany)
