245dceb824c989766e801bf3820f8e0a08589bdd
Carlos Blanco <carlosblancosierra@gmail.com>
2025-01-07 22:54:52 +0100

    Feat - Addresses fix

--- addresses/admin.py
+++ addresses/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+from .models import Address
 
-# Register your models here.
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'address_type', 'city', 'postcode', 'is_default', 'user']
+    list_filter = ['address_type', 'city', 'postcode']
+    search_fields = ['full_name', 'street_address', 'city', 'postcode']
+    raw_id_fields = ['user']
 
