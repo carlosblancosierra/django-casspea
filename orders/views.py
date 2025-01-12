7c1912fc4542695688db227e6929bcb8a85b50f7
Carlos Blanco <carlosblancosierra@gmail.com>
2025-01-12 15:50:32 +0100

    Fixes

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions, filters
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions
 from .models import Order
 from .serializers import OrderListSerializer
-from .filters import OrderFilter
 
 class OrderListView(generics.ListAPIView):
     """
@@ -12,12 +10,6 @@ class OrderListView(generics.ListAPIView):
     """
     serializer_class = OrderListSerializer
     permission_classes = [permissions.IsAdminUser]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_class = OrderFilter
-    search_fields = ['order_id', 'checkout_session__email',
-                    'checkout_session__shipping_address__full_name',
-                    'checkout_session__shipping_address__postcode']
-    ordering_fields = ['created', 'status', 'checkout_session__total_with_shipping']
     ordering = ['-created']
 
     def get_queryset(self):
