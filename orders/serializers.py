be5684db5f1e43a4bcf73af9e80f2f53abd3672f
Carlos Blanco <carlosblancosierra@gmail.com>
2025-01-12 15:54:35 +0100

    Feat - Orders Serializers

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,27 +1,20 @@
 from rest_framework import serializers
-from .models import Order, OrderStatusHistory
-from products.serializers import ProductSerializer
-from addresses.models import Address
+from .models import Order
+from checkout.models import CheckoutSession
+from addresses.serializers import AddressSerializer
+from carts.serializers import CartSerializer
 
-class AddressSerializer(serializers.ModelSerializer):
+class CheckoutSessionSerializer(serializers.ModelSerializer):
+    shipping_address = AddressSerializer()
+    billing_address = AddressSerializer()
+
+    cart = CartSerializer()
     class Meta:
-        model = Address
-        fields = '__all__'
-
-class CartItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = 'carts.CartItem'
-        fields = '__all__'
-
-
-class OrderStatusHistorySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = OrderStatusHistory
+        model = CheckoutSession
         fields = '__all__'
 
 class OrderListSerializer(serializers.ModelSerializer):
-
+    checkout_session = CheckoutSessionSerializer()
     class Meta:
         model = Order
         fields = '__all__'
